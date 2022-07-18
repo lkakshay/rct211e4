e0763eb9aae5287ea25c724767a95af4878a6ad9
lkakshay <connectme.lkakshay@gmail.com>
2022-07-18 17:05:51 +0530

    dfd

--- src/Redux/action.jsx
+++ src/Redux/action.jsx
@@ -2,7 +2,7 @@ import * as types from './actionTypes'
 import axios from 'axios'
 
 export const getCountries=()=>(dispatch)=>{
-   
+
      dispatch({type:types.GET_COUNTRIES_REQUEST})
      axios.get("http://localhost:8080/countries")
     .then((res)=>dispatch({type:types.GET_COUNTRIES_SUCCESS,payload:res.data}))
